180d3ead0ef3d893635ed8b06531f78f92e85040
Rui2003 <72198718+Rui2003@users.noreply.github.com>
2020-11-27 11:24:22 +0000

    Add files via upload

---
+++ testeredes/5.php
@@ -0,0 +1,31 @@
+<?php
+
+$meses = array(
+    1 => 'Janeiro',
+    2 =>'Fevereiro',
+    3 =>'Marco',
+    4 => 'Abril',
+    5 => 'Maio',
+    6 => 'Junho',
+    7 =>  'Julho',
+    8 =>   'Agosto',
+    9 =>  'Setembro',
+    10 => 'Outubro',
+    11 => 'Novembro',
+    12 => 'Dezembro'
+);
+
+
+$diassemana = array(
+     1 => 'Domingo',
+     2 => 'Segunda-Feira',
+     3 => 'Terça-Feira',
+     4 => 'Quarta-Feira',
+     5 => 'Quinta-Feira',
+     6 => 'Sexta-Feira',
+     7 => 'Sábado'
+);
+
+echo " O mês é ".$meses[$_POST['meses']]. " e o dia é : ".$diassemana[$_POST['dias']];
+
+?>
